ab4d11e39c38edf0aa2b6f3b90203aeb5f556486
Markus Rutström <markusrutstrom@gmail.com>
2022-02-16 20:11:52 +0100

    Taking and displaying picture

--- App.tsx
+++ App.tsx
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from "expo-camera";
+import { ImageType } from "expo-camera/build/Camera.types";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef, useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef<Camera>(null);
+
+  const [imageUri, setImageUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const toggleFlip = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+  const takePicture = async () => {
+    const picture = await cameraRef.current?.takePictureAsync({
+      imageType: ImageType.png,
+      quality: 0.5,
+    });
+    if (picture) setImageUri(picture.uri);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {imageUri && (
+        <View>
+          <Image source={{ uri: imageUri }} style={styles.image} />
+          <TouchableOpacity
+            style={styles.clear}
+            onPress={() => setImageUri(null)}
+          >
+            <Text style={styles.text}> Close </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {!imageUri && (
+        <Camera ref={cameraRef} style={styles.camera} type={type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.flip} onPress={toggleFlip}>
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.picture} onPress={takePicture}>
+              <Text style={styles.text}> Take </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
     </View>
   );
 }
@@ -13,8 +71,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "column",
+    margin: 50,
+    marginTop: 100,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  flip: {
+    flex: 0.1,
+    alignSelf: "flex-start",
+    alignItems: "center",
+  },
+  picture: {
+    width: 75,
+    height: 75,
+    backgroundColor: "orange",
+    borderColor: "white",
+    borderWidth: 5,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  clear: {
+    top: 100,
+    right: 50,
+    width: 75,
+    height: 75,
+    backgroundColor: "orange",
+    borderColor: "white",
+    borderWidth: 5,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
